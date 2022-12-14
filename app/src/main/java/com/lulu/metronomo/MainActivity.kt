e3c6e2780ade11680bad8131e3d9919f7a1728bf
luclucas <luclucas.08@gmail.com>
2022-12-13 22:52:15 -0300

    Implementar a seekbar

--- app/src/main/java/com/lulu/metronomo/MainActivity.kt
+++ app/src/main/java/com/lulu/metronomo/MainActivity.kt
@@ -7,8 +7,7 @@ import android.os.Looper
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
-import kotlin.concurrent.timerTask
+
 // https://www.cloverfield.co.jp/2021/06/10/%E3%80%90android%E3%80%91%E3%83%A1%E3%83%88%E3%83%AD%E3%83%8E%E3%83%BC%E3%83%A0%E3%82%A2%E3%83%97%E3%83%AA%E3%81%AE%E4%BD%9C%E6%88%90%E3%80%90kotlin%E3%80%91/
 
 /*
@@ -16,19 +15,22 @@ import kotlin.concurrent.timerTask
 * */
 class MainActivity : AppCompatActivity() {
     private lateinit var sp: SoundPool
-    private var batida = 0
+    private var spID = 0
     private lateinit var viewBPM: TextView
     private var isPlaying = false
     private var handler = Handler(Looper.getMainLooper())
+    private var valueBPM = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        sp = SoundPool.Builder().build()
-        criarSP()
-
         viewBPM = findViewById(R.id.textView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sp = SoundPool.Builder().build()
+        spID = sp.load(this, R.raw.tick, 1)
     }
 
     override fun onPause() {
@@ -36,34 +38,41 @@ class MainActivity : AppCompatActivity() {
         sp.release()
     }
 
-    fun aumentarBPM(v: View){
-        sp.stop(batida)
-        batida = 0
-        val texto = viewBPM.text.toString()
-        var valor = texto.toInt()
-        valor++
-        viewBPM.text = valor.toString()
+    fun increaseBPM(v: View) {
+        stop()
+        valueBPM = viewBPM.text.toString().toInt()
+        valueBPM++
+        viewBPM.text = valueBPM.toString()
     }
 
-    fun tocarAudio(v: View) {
+    fun decreaseBPM(v: View) {
+        stop()
+        valueBPM = viewBPM.text.toString().toInt()
+        valueBPM--
+        viewBPM.text = valueBPM.toString()
+    }
+
+    private fun stop(){
+        isPlaying = false
+        handler.removeCallbacks(runnable)
+    }
+
+    fun play(v: View) {
+        if(isPlaying){return}
         isPlaying = true
         handler.post(runnable)
     }
 
-    private fun retornaTempo():Long{
+    private fun retornaTempo(): Long {
         val tempoTexto = viewBPM.text.toString()
         val tempo = tempoTexto.toLong()
         val milisec = 60000
-        return milisec/tempo
+        return milisec / tempo
     }
 
-    private fun criarSP(){
-        batida = sp.load(this,R.raw.tick, 0)
-    }
-
-    private val runnable = object: Runnable{
+    private val runnable = object : Runnable {
         override fun run() {
-            sp.play(batida, 1f, 1f,100, 0,1f)
+            sp.play(spID, 1f, 1f, 100, 0, 1f)
             handler.postDelayed(this, retornaTempo())
         }
     }
